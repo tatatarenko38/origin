f1b319ad5cb1dd867a7f7ec8eeaefbc3b65457a6
tatatatarenko38@gmail.com <your_mail@example.com>
2023-11-03 12:50:41 +0200

    index83

--- src/route/index.js
+++ src/route/index.js
@@ -40,7 +40,7 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  res.render('/', {
     layout: "index",
     
   })
